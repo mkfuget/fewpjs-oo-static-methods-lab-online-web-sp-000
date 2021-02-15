8a1ed3e75fc3b5f89e5332b953b10db9fb0a9267
Michael Fuget <michaelfuget@gmail.com>
2021-02-15 01:48:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ class Formatter {
   static titleize(input)
   {
     return Formatter.capitalize(input.split(" ").map(function(x) {
-      if(!((x == "the") || (x == "a") || (x == "an") || (x == "but") ||  (x == "for") || (x == "at") || (x == "by") || (x == "from") || (x == "and") || (x == "and")))
+      if(!((x == "the") || (x == "a") || (x == "an") || (x == "but") ||  (x == "for") || (x == "at") || (x == "by") || (x == "from") || (x == "and") || (x == "of")))
       {
         return Formatter.capitalize(x)
       }
